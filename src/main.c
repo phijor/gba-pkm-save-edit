158fff7ff4a1817adc41dc925927cffb0c2a9299
Philipp Joram <phijor@t-online.de>
2015-06-20 20:24:13 +0200

    Add license-header.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,21 @@
+/* gba-pkm-save-edit -- A commandline application to edit save files of
+ *                      Pokémon-games for the GBA
+ * Copyright (C) <2015>  <Philipp Joram>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <getopt.h>
