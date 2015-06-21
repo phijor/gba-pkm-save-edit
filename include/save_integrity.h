442c99eb402ebb113c93660e8ceb20b404717e5f
Philipp Joram <phijor@t-online.de>
2015-06-21 14:47:37 +0200

    Add missing declaration of functions.

--- include/save_integrity.h
+++ include/save_integrity.h
@@ -3,12 +3,17 @@
 
 #include "save.h"
 
-int
-save_check_section_integrity(struct save_section_t* section);
+uint16_t save_checksum(void* buffer, size_t length);
 
-int
-save_check_block_integrity(struct save_block_t* save_block);
+int save_check_section_validation_code(struct save_section_t* section);
+int save_check_section_checksum_integrity(struct save_section_t* section);
 
-int
-save_check_file_integrity(struct save_file_t* save_file);
+int save_check_section_integrity(struct save_section_t* section);
+int save_check_block_integrity(struct save_block_t* save_block);
+int save_check_file_integrity(struct save_file_t* save_file);
+
+int save_resign_section(struct save_section_t* section);
+int save_resign_block(struct save_block_t* block);
+int save_resign_file(struct save_file_t* file);
+
 #endif
