f465643713c2b19be2342809007a11b42c351acd
Philipp Joram <phijor@t-online.de>
2015-06-22 23:50:02 +0200

    Realign data and add box information.

--- include/save_ruby_sapphire.h
+++ include/save_ruby_sapphire.h
@@ -6,6 +6,7 @@
 #include "save_common.h"
 #include "save_pokedex_types.h"
 #include "save_pokemon.h"
+#include "save_boxes.h"
 
 struct __attribute__((packed)) save_ruby_sapphire_items_t {
     struct save_item_t pc_storage[50];
@@ -30,26 +31,29 @@ struct __attribute__((packed)) save_ruby_sapphire_t {
     uint8_t _3[(0x00ac - 0x005c) - sizeof(struct save_pokedex_t)];
 
     uint32_t game_code;
-    uint8_t _4[(0x1160 - 0x00ac) - sizeof(uint32_t)];
+    uint8_t _4[(0x1228 - 0x00ac) - sizeof(uint32_t)];
 
     uint32_t team_size;
     struct save_pokemon_t team_pokemon[6];
     uint32_t money;
-    uint8_t _5[(0x1418 - 0x1160) - 2*sizeof(uint32_t) - 6*sizeof(struct save_pokemon_t)];
+    uint8_t _5[(0x148c - 0x1228) - 2*sizeof(uint32_t) - 6*sizeof(struct save_pokemon_t)];
 
     struct save_ruby_sapphire_items_t items;
-    uint8_t _6[(0x1864 - 0x1418) - sizeof(struct save_ruby_sapphire_items_t)];
+    uint8_t _6[(0x192c - 0x148c) - sizeof(struct save_ruby_sapphire_items_t)];
 
     struct save_pokedex_t pokedex_seen_b;
-    uint8_t _7[(0x2252 - 0x1864) - sizeof(struct save_pokedex_t)];
+    uint8_t _7[(0x238e - 0x192c) - sizeof(struct save_pokedex_t)];
     
     uint8_t pokedex_national_unlocked_b;
-    uint8_t _8[(0x22f8 - 0x2252) - sizeof(uint8_t)];
+    uint8_t _8[(0x2434 - 0x238e) - sizeof(uint8_t)];
 
     uint16_t pokedex_national_unlocked_c;
-    uint8_t _9[(0x49b8 - 0x22f8) - sizeof(uint16_t)];
+    uint8_t _9[(0x4bdc - 0x2434) - sizeof(uint16_t)];
 
     struct save_pokedex_t pokedex_seen_c;
-    uint8_t _10[(0xd084 - 0x49b8) - sizeof(struct save_pokedex_t)];
+    uint8_t _10[(0x4fc4 - 0x4bdc) - sizeof(struct save_pokedex_t)];
+
+    struct save_box_data_t boxes;
+    //last byte at 0xdf58
 };
 #endif
