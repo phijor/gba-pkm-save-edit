52865112bf4820eca8691d1b449b934c8b81e782
Philipp Joram <phijor@t-online.de>
2015-06-26 11:07:48 +0200

    Close file after dumping to it.

--- src/editor_dump.c
+++ src/editor_dump.c
@@ -10,5 +10,6 @@ int editor_dump(union save_unpacked_t* save, int argc, char* const* argv) {
     fprintf(stderr, "Writing to \"%s\".", argv[0]);
     FILE* dump_file = fopen(argv[0], "w");
     fwrite(save, sizeof(union save_unpacked_t), 1, dump_file);
+    fclose(dump_file);
     return EXIT_SUCCESS;
 }
