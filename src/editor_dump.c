cb0b4e547f8ac9d8c935f60f90a415bd9b13626f
Philipp Joram <phijor@t-online.de>
2015-07-21 16:22:32 +0200

    Error-check first argument for filename.
    If no filename is given, issue a warning and dump to a default name.

--- src/editor_dump.c
+++ src/editor_dump.c
@@ -7,9 +7,17 @@
 #include "save_unpacked.h"
 
 int editor_dump(union save_unpacked_t* save, int argc, char* const* argv) {
-    fprintf(stderr, "Writing to \"%s\".", argv[0]);
-    FILE* dump_file = fopen(argv[0], "w");
+    char* dump_file_name = argv[0];
+
+    if (dump_file_name == NULL) {
+        fprintf(stderr, "Missing file name.\n");
+        dump_file_name = "dump.sav.unpacked";
+    }
+    fprintf(stdout, "Writing to \'%s\'.", dump_file_name);
+
+    FILE* dump_file = fopen(dump_file_name, "w");
     fwrite(save, sizeof(union save_unpacked_t), 1, dump_file);
     fclose(dump_file);
+
     return EXIT_SUCCESS;
 }
