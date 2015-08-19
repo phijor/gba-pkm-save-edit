2a3662165ad46e49b8b0539448393e17cc9268c2
Philipp Joram <phijor@t-online.de>
2015-08-20 00:22:15 +0200

    Export each team-Pokemon to a seperate file.

--- src/editor_export.c
+++ src/editor_export.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
 
 #include "message.h"
 #include "editor_export.h"
 #include "editor.h"
+#include "editor_parse.h"
 #include "save_unpacked.h"
+#include "save_pokemon.h"
 #include "save_pokemon_storage.h"
 
 int editor_export(union save_unpacked_t* save, int argc, char* const* argv) {
@@ -27,22 +30,43 @@ int editor_export_pokemon(union save_unpacked_t* save, int argc,
 
 int editor_export_pokemon_team(union save_unpacked_t* save, int argc,
                                char* const* argv) {
-    char* dump_file_name = argv[0];
-
-    if (argc < 1 || dump_file_name == NULL) {
-        message("E", "Missing file name.\n");
-        dump_file_name = "dump.pkm.lst";
+    struct editor_range_t range = {
+        .min = 0, .max = 5,
+    };
+    if (argc >= 1 && argv[0] != NULL) {
+        editor_parse_range(&range, argv[0]);
+    } else {
+        range.lower = range.min;
+        range.upper = range.max;
     }
-    message("I", "Writing to \'%s\'.\n", dump_file_name);
-    message("W",
-            "This function currently outputs the whole team as one file.\n");
+    message("I", "Exporting Pokemon from team slot(s) %d to %d.\n",
+            range.lower + 1, range.upper + 1);
+
+#define EXPORT_NICK "___name___"
+#define EXPORT_OT   "___OT__"
+#define EXPORT_PID  "___PID__"
+    char export_file_names[6][sizeof(EXPORT_NICK "-" EXPORT_PID "-" EXPORT_OT
+                                                 "-_.pkm")] = {'\0'};
 
     struct save_pokemon_boxed_t team[6];
     save_pokemon_get_team(save, team);
 
-    FILE* dump_file = fopen(dump_file_name, "w");
-    fwrite(team, sizeof(team), 1, dump_file);
-    fclose(dump_file);
+    for (ssize_t i = range.lower; i <= range.upper; i++) {
+        struct save_pokemon_boxed_t* current = &team[i];
+
+        char nickname[sizeof(((struct save_pokemon_boxed_t*)0)->nickname)];
+        save_pokemon_get_nickname(current, nickname);
+
+        char ot_name[sizeof(((struct save_pokemon_boxed_t*)0)->OT_name)];
+        save_pokemon_get_ot_name(current, ot_name);
+
+        snprintf(export_file_names[i], sizeof(export_file_names[0]),
+                 "%s-%08x-%s-%ld.pkm", nickname, current->PID, ot_name, i + 1);
+
+        FILE* export_file = fopen(export_file_names[i], "w");
+        fwrite(current, sizeof(struct save_pokemon_boxed_t), 1, export_file);
+        fclose(export_file);
+    }
 
     return EXIT_SUCCESS;
 }
