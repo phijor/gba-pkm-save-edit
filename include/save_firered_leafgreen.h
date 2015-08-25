bbb4a47e634d386e4a0081db59855cfe83783fcb
Philipp Joram <phijor@t-online.de>
2015-08-25 13:43:40 +0200

    Fix offsets in Firered/Leafgreen save structure.

--- include/save_firered_leafgreen.h
+++ include/save_firered_leafgreen.h
@@ -19,6 +19,7 @@ struct __attribute__((packed)) save_firered_leafgreen_items_t {
 
 struct save_firered_leafgreen_t {
     struct save_trainer_info_t player;
+    //buffer:  (next - previous) - previous_size
     uint8_t _0[(0x001b - 0x0000) - sizeof(struct save_trainer_info_t)];
 
     uint8_t pokedex_national_unlocked_a;
@@ -34,34 +35,34 @@ struct save_firered_leafgreen_t {
     uint8_t _4[(0x0af8 - 0x00ac) - sizeof(uint32_t)];
 
     uint32_t security_key;
-    uint8_t _5[(0x1028 - 0x0af8) - sizeof(uint32_t)];
+    uint8_t _5[(0x0f60 - 0x0af8) - sizeof(uint32_t)];
 
     uint32_t party_size;
     struct save_pokemon_t party[6];
     uint32_t money;
-    uint8_t _6[(0x128c - 0x1028) - 2 * sizeof(uint32_t) -
-               6*sizeof(struct save_pokemon_t)];
+    uint16_t coins;
+    uint8_t _6[(0x11c4 - 0x0f60) - 2 * sizeof(uint32_t) -
+               sizeof(uint16_t) - 6*sizeof(struct save_pokemon_t)];
 
     struct save_firered_leafgreen_items_t items;
-    uint8_t _7[(0x15ec - 0x1228) - sizeof(struct save_firered_leafgreen_items_t)];
+    uint8_t _7[(0x1524 - 0x11c4) - sizeof(struct save_firered_leafgreen_items_t)];
 
     struct save_pokedex_t pokedex_seen_b;
-    uint8_t _8[(0x1f68 - 0x15ec) - sizeof(struct save_pokedex_t)];
+    uint8_t _8[(0x1f14 - 0x1524) - sizeof(struct save_pokedex_t)];
 
     uint8_t pokedex_national_unlocked_b;
-    uint8_t _9[(0x2014 - 0x1f68) - sizeof(uint8_t)];
+    uint8_t _9[(0x1fc8 - 0x1f14) - sizeof(uint8_t)];
 
     uint16_t pokedex_national_unlocked_c;
-    uint8_t _10[(0x4b68 - 0x2014) - sizeof(uint16_t)];
+    uint8_t _10[(0x4944 - 0x1fc8) - sizeof(uint16_t)];
 
     struct save_pokedex_t pokedex_seen_c;
-    uint8_t _11[(0x4b9c - 0x4b68) - sizeof(struct save_pokedex_t)];
+    uint8_t _11[(0x4978 - 0x4944) - sizeof(struct save_pokedex_t)];
 
     uint8_t rival_name[8];
-    uint8_t _12[(0x4fc4 - 0x4b9c) - 8*sizeof(uint8_t)];
+    uint8_t _12[(0x4cb4 - 0x4978) - 8*sizeof(uint8_t)];
 
     struct save_box_data_t boxes;
-    //last byte at 0xdf58
 };
 
 #endif
