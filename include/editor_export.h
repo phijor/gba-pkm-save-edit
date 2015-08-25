c3d330994408667397cde40c6df494573a2190da
Philipp Joram <phijor@t-online.de>
2015-08-25 23:30:04 +0200

    Add functionality to export Pokémon from boxes.

--- include/editor_export.h
+++ include/editor_export.h
@@ -2,6 +2,7 @@
 #define EDITOR_EXPORT_H
 
 union save_unpacked_t;
+struct save_pokemon_boxed_t;
 
 int editor_export(union save_unpacked_t* save, int argc, char* const* argv);
 
@@ -10,5 +11,11 @@ int editor_export_pokemon(union save_unpacked_t* save, int argc,
 
 int editor_export_pokemon_party(union save_unpacked_t* save, int argc,
                                char* const* argv);
+
+int editor_export_pokemon_box(union save_unpacked_t* save, int argc,
+                               char* const* argv);
+
+int editor_export_pokemon_write(struct save_pokemon_boxed_t* pokemon,
+                                char* dir_path, char* prefix);
 
 #endif /* ----- #ifndef EDITOR_EXPORT_H ----- */
--- src/editor_export.c
+++ src/editor_export.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 
 #include "message.h"
@@ -22,7 +23,7 @@ int editor_export_pokemon(union save_unpacked_t* save, int argc,
                           char* const* argv) {
     const struct editor_call_t commands[] = {
         {.name = "party", .exec = &editor_export_pokemon_party},
-        {.name = "box", .exec = NULL},
+        {.name = "box", .exec = &editor_export_pokemon_box},
         {.name = NULL, .exec = NULL},
     };
     return editor_call(save, commands, argc, argv);
@@ -30,14 +31,23 @@ int editor_export_pokemon(union save_unpacked_t* save, int argc,
 
 int editor_export_pokemon_party(union save_unpacked_t* save, int argc,
                                char* const* argv) {
+    int optind = 0;
     struct editor_range_t range = {
         .min = 1, .max = SAVE_PARTY_SLOTS,
     };
-    if (argc >= 1 && argv[0] != NULL) {
-        editor_parse_range(&range, argv[0]);
+    char* directory = NULL;
+
+    if (argc > optind && argv[optind] != NULL) {
+        editor_parse_range(&range, argv[optind]);
+        optind++;
     } else {
         range.lower = range.min;
         range.upper = range.max;
+    }
+    if (argc > optind && argv[optind] != NULL) {
+        directory = argv[optind];
+    } else {
+        directory = ".";
     }
 
     long int party_size = save_pokemon_get_party_size(save);
@@ -49,34 +59,85 @@ int editor_export_pokemon_party(union save_unpacked_t* save, int argc,
     message("I", "Exporting Pokemon from party slot(s) %d to %d.\n",
             range.lower, range.upper);
 
-    char export_file_names[SAVE_PARTY_SLOTS][sizeof("~-[NAME~~~~]-0x[PID~~~]-[OT~~~].g3pkm")] = {'\0'};
-
     struct save_pokemon_boxed_t party[SAVE_PARTY_SLOTS];
     save_pokemon_get_party(save, party);
 
     for (ssize_t i = range.lower; i <= range.upper; i++) {
-        struct save_pokemon_boxed_t* current = &party[i - 1];
-        char* current_file_name = export_file_names[i - 1];
-
-        if (save_pokemon_slot_is_empty(current)) {
-            message("W", "Slot %ld is empty; skipping slot.\n", i);
-            continue;
-        }
-
-        char nickname[sizeof(((struct save_pokemon_boxed_t*)0)->nickname)];
-        save_pokemon_get_nickname(current, nickname);
-
-        char ot_name[sizeof(((struct save_pokemon_boxed_t*)0)->OT_name)];
-        save_pokemon_get_ot_name(current, ot_name);
-
-        snprintf(current_file_name, sizeof(export_file_names[0]),
-                 "%ld-%s-%#08x-%s.g3pkm", i, nickname, current->PID, ot_name);
-
-        message("I", "Writing slot %ld to \'%s\'.\n", i, current_file_name);
-        FILE* export_file = fopen(current_file_name, "w");
-        fwrite(current, sizeof(struct save_pokemon_boxed_t), 1, export_file);
-        fclose(export_file);
+        char prefix[2];
+        snprintf(prefix, 2, "%1ld", i);
+        editor_export_pokemon_write(&party[i - 1], directory, prefix);
     }
 
+    return EXIT_SUCCESS;
+}
+
+int editor_export_pokemon_box(union save_unpacked_t* save, int argc,
+                               char* const* argv) {
+    int optind = 0;
+    struct editor_range_t boxes = {
+        .min = 1, .max = SAVE_BOXES,
+    };
+    struct editor_range_t slots = {
+        .min = 1, .max = SAVE_BOX_SLOTS,
+    };
+    char* directory = NULL;
+
+    if (argc > optind && argv[optind] != NULL) {
+        editor_parse_range(&boxes, argv[optind]);
+        optind++;
+    } else {
+        boxes.lower = boxes.min;
+        boxes.upper = boxes.max;
+    }
+    if (argc > optind && argv[optind] != NULL) {
+        editor_parse_range(&slots, argv[optind]);
+        optind++;
+    } else {
+        slots.lower = slots.min;
+        slots.upper = slots.max;
+    }
+    if (argc > optind && argv[optind] != NULL) {
+        directory = argv[optind];
+    } else {
+        directory = ".";
+    }
+
+    message("I", "Exporting Pokemon from boxes %d to %d, slots %d to %d each.\n",
+            boxes.lower, boxes.upper, slots.lower, slots.upper);
+
+    for (ssize_t box_ind = boxes.lower; box_ind <= boxes.upper; box_ind++) {
+        struct save_box_unpacked_t box;
+        save_pokemon_get_box(save, box_ind - 1, &box);
+        for (ssize_t slot = slots.lower; slot <= slots.upper; slot++) {
+            char prefix[6];
+            snprintf(prefix, 6, "%02ld-%02ld", box_ind, slot);
+            editor_export_pokemon_write(&box.pokemon[slot - 1], directory, prefix);
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+int editor_export_pokemon_write(struct save_pokemon_boxed_t* pokemon,
+                                char* dir_path, char* prefix) {
+    size_t file_name_size = strlen(dir_path) + sizeof('/') + strlen(prefix) +
+                            sizeof("-[NAME~~~~]-[OT~~~]-0x[PID~~~].g3pkm");
+    char* file_name = malloc(file_name_size);
+    if (save_pokemon_slot_is_empty(pokemon)) {
+        return EXIT_FAILURE;
+    }
+
+    char nickname[SAVE_POKEMON_NICKNAME_SIZE];
+    save_pokemon_get_nickname(pokemon, nickname);
+
+    char ot_name[SAVE_TRAINER_NAME_SIZE];
+    save_pokemon_get_ot_name(pokemon, ot_name);
+
+    snprintf(file_name, file_name_size, "%s/%s-%s-%s-%#08x.g3pkm",
+             dir_path, prefix, nickname, ot_name, pokemon->PID);
+
+    message("I", "Writing Pokemon to \'%s\'.\n", file_name);
+    FILE* export_file = fopen(file_name, "w");
+    fwrite(pokemon, sizeof(struct save_pokemon_boxed_t), 1, export_file);
+    fclose(export_file);
     return EXIT_SUCCESS;
 }
