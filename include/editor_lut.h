904fbea2c23817ae03f4def995f0b0852234d34d
Philipp Joram <phijor@t-online.de>
2015-09-03 02:58:53 +0200

    Add editor LUT containing the names of all species.

--- include/editor_lut.h
+++ include/editor_lut.h
@@ -2,7 +2,10 @@
 #define  EDITOR_LUT_H
 
 #include "save_moves.h"
+#include "save_species.h"
 
 char* editor_move_names[SAVE_MOVES];
+
+char* editor_species_names[SAVE_SPECIES];
 
 #endif   /* ----- #ifndef EDITOR_LUT_H  ----- */
