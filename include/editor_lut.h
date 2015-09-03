f75e6582e5c3956eb8f7fac14f9d5598495eca5d
Philipp Joram <phijor@t-online.de>
2015-09-03 21:51:16 +0200

    Add editor LUT containing the names of all items.

--- include/editor_lut.h
+++ include/editor_lut.h
@@ -3,9 +3,12 @@
 
 #include "save_moves.h"
 #include "save_species.h"
+#include "save_items.h"
 
 char* editor_move_names[SAVE_MOVES];
 
 char* editor_species_names[SAVE_SPECIES];
+
+char* editor_item_names[SAVE_ITEMS];
 
 #endif   /* ----- #ifndef EDITOR_LUT_H  ----- */
