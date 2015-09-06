ced6e2a1117030f85921e8229ed05aae1f3ec734
Philipp Joram <phijor@t-online.de>
2015-09-06 16:25:30 +0200

    Add editor LUT containing the names of all natures.

--- include/editor_lut.h
+++ include/editor_lut.h
@@ -4,11 +4,14 @@
 #include "save_moves.h"
 #include "save_species.h"
 #include "save_items.h"
+#include "save_natures.h"
 
 char* editor_move_names[SAVE_MOVES];
 
 char* editor_species_names[SAVE_SPECIES];
 
 char* editor_item_names[SAVE_ITEMS];
+
+char* editor_nature_names[SAVE_NATURES];
 
 #endif   /* ----- #ifndef EDITOR_LUT_H  ----- */
