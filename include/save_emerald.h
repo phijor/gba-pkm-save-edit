7024c8fed3a5551cce999b47e0aab47570a1030a
Philipp Joram <phijor@t-online.de>
2015-09-09 19:45:09 +0200

    Add Walda's phrase to save structure.

--- include/save_emerald.h
+++ include/save_emerald.h
@@ -10,6 +10,8 @@
 #include "save_storage.h"
 #include "save_link.h"
 
+#define SAVE_WALDAS_PHRASE_LENGTH 16
+
 struct save_emerald_items_t {
     struct save_item_t pc_storage[50];
     struct save_item_t item_pocket[30];
@@ -65,7 +67,10 @@ struct save_emerald_t {
     uint8_t _15[(0x4a50 - 0x407c) - sizeof(struct save_pokemon_t)];
 
     struct save_pokedex_t pokedex_seen_c;
-    uint8_t _11[(0x4cb4 - 0x4a50) - sizeof(struct save_pokedex_t)];
+    uint8_t _11[(0x4ca0 - 0x4a50) - sizeof(struct save_pokedex_t)];
+
+    save_char_t waldas_phrase[SAVE_WALDAS_PHRASE_LENGTH];
+    uint8_t _14[(0x4cb4 - 0x4ca0) - sizeof(save_char_t) * SAVE_WALDAS_PHRASE_LENGTH];
 
     struct save_box_data_t boxes;
     //last byte at 0xdf58
